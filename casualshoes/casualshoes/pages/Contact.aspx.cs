369e5724cee806707790789f8bab54c9675d0ed3
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-09-25 20:09:07 +0200

    kontakt form

--- casualshoes/casualshoes/pages/Contact.aspx.cs
+++ casualshoes/casualshoes/pages/Contact.aspx.cs
@@ -5,6 +5,9 @@ using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net;
+
+
 
 namespace casualshoes
 {
@@ -15,27 +18,54 @@ namespace casualshoes
 
         }
         //TODO: FIX bug unobtrusive validation mode error in this code
-        protected void btn_sendMail_Click(object sender, EventArgs e)
-        {
-            
+        //protected void btn_sendMail_Click(object sender, EventArgs e)
+        //{
 
-            if (Page.IsValid==true)
-            {
-                using (MailMessage message = new MailMessage(txt_email.Text.ToString(),"dalius_pamparas@gmail.com","this is a subject by "+txt_fullname.Text,txt_comments.Text))
-                {
-                    SmtpClient client = new SmtpClient();
-                    client.Host = "smtp.gmail.com";
-                    client.Send(message);
-                }
-                lbl_sentMessage.Text = $"Tack {txt_fullname.Text}, vi ska svara så snabt som möjligt";
-            }
-            else
-            {
-                lbl_sentMessage.Text = "Du har inte fyllt i formuläret fullständigt!";
-            }
+
+        //    if (Page.IsValid==true)
+        //    {
+        //        using (MailMessage message = new MailMessage(txt_email.Text.ToString(),"dalius_pamparas@gmail.com","this is a subject by "+txt_fullname.Text,txt_comments.Text))
+        //        {
+        //            SmtpClient client = new SmtpClient();
+        //            client.Host = "smtp.gmail.com";
+        //            client.Send(message);
+        //        }
+        //        lbl_sentMessage.Text = $"Tack {txt_fullname.Text}, vi ska svara så snabt som möjligt";
+        //    }
+        //    else
+        //    {
+        //        lbl_sentMessage.Text = "Du har inte fyllt i formuläret fullständigt!";
+        //    }
+
+        //}
+        -------------------------------------------------------
+        public void btnSend_Click1(object sender, EventArgs e)
+        {
+           
+            MailMessage msg = new MailMessage();
+            msg.To.Add(new MailAddress("projekt.eshop@gmail.com"));
+            msg.From = new MailAddress(txtEmail.Text);
+            msg.Subject = txtSubject.Text;
+            msg.Body = txtName.Text + Environment.NewLine + txtMessage.Value;
+            SmtpClient client = new SmtpClient();
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.Credentials = new NetworkCredential("projekt.eshop","qazqaz123");
+            client.EnableSsl = true;
+            client.Send(msg);
+           
 
         }
+      
 
-
+        protected void btnReset_Click1(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            txtEmail.Text = "";
+            txtSubject.Text = "";
+            txtMessage.Value = "";
+        }
     }
 }
