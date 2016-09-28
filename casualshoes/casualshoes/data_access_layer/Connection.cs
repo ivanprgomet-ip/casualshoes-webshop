b13c9f62b20b3771412ee4c84d1448c3ef18f768
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-09-28 09:16:01 +0200

    added separate connectionsclass and productDAL class for handling database related activities

---
+++ casualshoes/casualshoes/data_access_layer/Connection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace casualshoes.data_access_layer
+{
+    public static class Connection
+    {
+        private static SqlConnection conn;
+        private static SqlCommand comm;
+
+        static Connection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["CasualShoesDBConnectionString"].ToString();
+            conn = new SqlConnection(connectionString);
+            comm = new SqlCommand("", conn);
+        }
+    }
+}
---
+++ casualshoes/casualshoes/data_access_layer/ProductDAL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace casualshoes.data_access_layer
+{
+    public class ProductDAL
+    {
+
+
+
+
+
+        public static ArrayList GetProductsByCategory(string productCategory)
+        {
+            ArrayList list = new ArrayList();
+            string sqlQuery = string.Format(@"SELECT Product.ProductId,Product.ProductName,product.ProductPrice, Brand.BrandName, Size.Size, Category.CategoryName
+                                            FROM Brand INNER JOIN
+                                            Product ON Brand.BrandId = Product.ProductBrandId INNER JOIN
+                                            Category ON Product.ProductCategoryId = Category.CategoryId INNER JOIN
+                                            Size ON Product.ProductSizeId = Size.SizeId
+                                            WHERE CategoryName LIKE 'Sneakers'");
+
+            try
+            {
+                Connection.conn.Open();
+                Connection.comm.CommandText = sqlQuery;
+                SqlDataReader reader = Connection.comm.ExecuteReader();
+
+                while(reader.Read())//while reader returns true there are rows to be read
+                {
+                    int id = reader.GetInt32(0);
+                    string productName = reader.GetString(1);
+                    float productPrice = reader.GetFloat(2);
+                    string brandName = reader.GetString(3);
+                    string productSize = reader.GetString(4);
+                    string categoryName = reader.GetString(5); 
+                }
+            }
+            finally
+            {
+                Connection.conn.Close();
+            }
+        }
+    }
+}
