7a942a4e4473beb72bd79727ddfd9a3c24b98f2a
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-09-30 21:48:48 +0200

    added redirect to login page for persons that try to add product to cart without being logged in first. using sessionstate

--- casualshoes/casualshoes/pages/ProductDetails.aspx.cs
+++ casualshoes/casualshoes/pages/ProductDetails.aspx.cs
@@ -22,22 +22,29 @@ namespace casualshoes
 
         protected void btn_addToCart_Command(object sender, CommandEventArgs e)
         {
+            if((string)Session["login"] == null)
+            {
+                //redirects anonymous customers to login before adding to cart
+                Response.Redirect("~/pages/Login.aspx");
+            }
+            else //if someone is logged in
+            {
+                //CREATE AN ORDERID FOR THE CUSTOMERID THAT CLICKED THE BUTTON
 
-            //CHECK IF ANY (USER) IS LOGGED IN 
-            //CREATE AN ORDERID FOR THE CUSTOMERID THAT CLICKED THE BUTTON
+                //RETRIEVE VALUES FROM CONTROLS TO ADD TO CART
+                List<string> commandArgs = e.CommandArgument.ToString().Split(new char[] { ',' }).ToList();
+                int orderid = 4;//ONLY TESTING PURPOSES
+                int productid = Convert.ToInt32(commandArgs[0]);
+                string productname = commandArgs[1];
+                int quantity = Convert.ToInt32(num_quantity.Value);
+                int modelsize = Convert.ToInt32(ddl_modelsize.SelectedValue);
+                OrderDetail newOrderDetail = new OrderDetail(orderid, productid, quantity, modelsize);
+
+                OrderDetailDAL.AddNewOrderDetail(newOrderDetail);
+
+                Response.Write($"orderid:{orderid}, productid: {productid}, quantity: {quantity}, modelsize: {modelsize}");
+            }
             
-            List<string> commandArgs = e.CommandArgument.ToString().Split(new char[] { ',' }).ToList();
-
-            int orderid = 4;//ONLY TESTING PURPOSES
-            int productid = Convert.ToInt32(commandArgs[0]);
-            string productname = commandArgs[1];
-            int quantity = Convert.ToInt32(num_quantity.Value);
-            int modelsize = Convert.ToInt32(ddl_modelsize.SelectedValue);
-            OrderDetail newOrderDetail = new OrderDetail(orderid, productid, quantity, modelsize);
-
-            OrderDetailDAL.AddNewOrderDetail(newOrderDetail);
-
-            Response.Write($"orderid:{orderid}, productid: {productid}, quantity: {quantity}, modelsize: {modelsize}");
         }
     }
 }
